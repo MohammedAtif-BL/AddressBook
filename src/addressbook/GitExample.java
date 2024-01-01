98daea6c2f39f2f24baac41db68688155301b88e
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-01-01 10:30:00 +0530

    Updated Java code

--- src/addressbook/GitExample.java
+++ src/addressbook/GitExample.java
@@ -6,5 +6,8 @@ public class GitExample {
         System.out.println("You can use this code to practice Git commands like commit, push, and pull.");
         System.out.println("Make sure to create a repository on GitHub and link it with your local repository.");
         System.out.println("Happy coding!");
+        System.out.println("Remember to commit your changes frequently and write meaningful commit messages.");
+        System.out.println("You can also create branches to work on new features or fixes without affecting");
+        System.out.println("the main codebase. Use 'git branch' to create a new branch and 'git checkout' to switch to it.");
     }
 }
