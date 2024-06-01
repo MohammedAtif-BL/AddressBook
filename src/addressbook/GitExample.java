a6fca0a35e94e892ca66cf9b8bc0ac3031bd54bf
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-06-01 10:30:00 +0530

    Initial commit with Java code

--- src/addressbook/GitExample.java
+++ src/addressbook/GitExample.java
@@ -1,4 +1,10 @@
 package addressbook;
 
 public class GitExample {
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program to demonstrate Git usage.");
+        System.out.println("You can use this code to practice Git commands like commit, push, and pull.");
+        System.out.println("Make sure to create a repository on GitHub and link it with your local repository.");
+        System.out.println("Happy coding!");
+    }
 }
