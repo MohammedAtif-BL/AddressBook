8f0755def5236a9bedaa53cf1572edb2b2555efb
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2025-08-18 17:11:46 +0530

    Added New Contact

---
+++ src/addressbook/AddressBookService.java
@@ -0,0 +1,8 @@
+package addressbook;
+
+public class AddressBookService {
+    public void addContact(Contact contact) {
+        // Logic to add contact to the address book
+        System.out.println("Adding contact: ");
+    }
+}
